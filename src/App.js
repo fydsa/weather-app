3aa4ef7ecd8e8ffb494c939615a151910ad43c2a
fydsa <saba.alikhani@ymail.com>
2022-01-18 23:18:04 +0330

    Added the footer to the app component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from "./logo.svg";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/header/header.component";
+import Footer from "./components/footer/footer.component";
 
 function App() {
   return (
     <Router>
       <Header />
+      <Footer />
     </Router>
   );
 }
